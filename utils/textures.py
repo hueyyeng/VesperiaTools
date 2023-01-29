285269bca0b468d2d911cc5130237236cb185f56
Huey Yeng <huey.yeng.mmu@gmail.com>
2023-01-29 16:15:34 +0800

    Hotfix: Use Path for textures operation

--- utils/textures.py
+++ utils/textures.py
@@ -9,7 +9,6 @@ logger = logging.getLogger(__name__)
 
 def write_to_dds(node: Node, output_path: str):
     output_path = Path(output_path)
-    output_path.mkdir(exist_ok=True)
 
     if node.name != 'NONAME':
         output_path = output_path / node.name
@@ -17,8 +16,8 @@ def write_to_dds(node: Node, output_path: str):
     for image in node.data["image_list"]:
         texture_name = image["texture_name"]
         texture_path = output_path / texture_name
+        texture_path.parent.mkdir(exist_ok=True)
         dds_content = image["dds_content"]
-        with open(texture_path, 'wb') as dds_file:
-            dds_file.write(dds_content)
+        texture_path.write_bytes(dds_content)
 
     logger.info("Writing DDS textures completed.")
